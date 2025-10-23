c7eaf581dda08083d0e22a251a043b27886a6732
Ratanmani <ratanmanisingh553@gmail.com>
2025-10-23 13:35:13 +0530

    Initial commit

---
+++ operators.py
@@ -0,0 +1,30 @@
+# Addition
+a = 10
+b = 11
+print(a+b)
+
+# Subtraction
+a = int(input("Enter any number"))
+b = int(input("Enter any number"))
+c = a - b
+print(c)
+
+# Multiply
+a = 10 
+b = 5
+print(a*b)
+
+# Division
+a = 24
+b = 3
+print(a/b)
+
+# floor division
+a = 25
+b = 2
+print(a//b)
+
+# Modules
+n = 30
+m = 7
+print(n%m)
