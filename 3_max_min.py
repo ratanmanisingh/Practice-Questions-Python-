3528130def3459868547f8c1bd357d0a4362a2e7
Ratanmani <ratanmanisingh553@gmail.com>
2025-10-23 14:03:07 +0530

    change

---
+++ 3_max_min.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+c = int(input("Enter your third number: "))
+
+maximum = max(a, b, c)
+minimum = min(a, b, c)
+
+print("The maximum number is:", maximum)
+print("The minimum number is:", minimum)
---
+++ 4_temperature.py
@@ -0,0 +1,3 @@
+temperature = float(input("Enter the temperature in Celsius: "))
+fahrenheit = (temperature * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {fahrenheit}")
