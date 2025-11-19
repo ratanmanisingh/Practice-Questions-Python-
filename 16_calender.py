9285965327d7f3e317d27d7fa460256ec1c36d9e
Ratanmani <ratanmanisingh553@gmail.com>
2025-11-20 00:45:56 +0530

    add

--- 16_calender.py
+++ 16_calender.py
@@ -1,5 +1,13 @@
-import calendar
-# print(calendar.month(2024, 1)) # January 2024
+# import calendar
+# # print(calendar.month(2024, 1)) # January 2024
 
-for i in range(1,13):
-    print(calendar.month(2025,i))
+# for i in range(1,13):
+#     print(calendar.month(2025,i))
+
+
+import calendar
+
+month,day,year = map(int,input().split())
+day = calendar.weekday(year,month,day)
+day_upper = calendar.day_name[day].upper()
+print(day_upper)
